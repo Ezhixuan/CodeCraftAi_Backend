12f2bada39404a89449c0c07b903be7abc75fe63
Ezhixuan <ezhixuanna@163.com>
2025-08-07 18:16:04 +0800

    fix(Exception): 清除无效的用户登录状态
    - 在全局异常处理器中增加对 401错误的特殊处理, 当发生 401 错误时，检查并清除用户的登录状态

--- src/main/java/com/ezhixuan/ai/codeCraftAi_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ezhixuan/ai/codeCraftAi_backend/exception/GlobalExceptionHandler.java
@@ -7,11 +7,16 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.ezhixuan.ai.codeCraftAi_backend.common.BaseResponse;
 import com.ezhixuan.ai.codeCraftAi_backend.common.R;
+import com.ezhixuan.ai.codeCraftAi_backend.domain.constant.UserConstant;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
@@ -30,13 +35,19 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(BusinessException.class)
-    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+    public BaseResponse<String> businessExceptionHandler(BusinessException e) {
+        if (String.valueOf(e.getCode()).startsWith("401")) {
+            // 需要检查是否存在登录态,如果存在需要清除
+            RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+            HttpServletRequest request = ((ServletRequestAttributes)requestAttributes).getRequest();
+            request.getSession().removeAttribute(UserConstant.USER_LOGIN_STATE);
+        }
         log.error("businessException: {}", e.getMessage());
         return R.error(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public BaseResponse<?> exceptionHandler(Exception e) {
+    public BaseResponse<String> exceptionHandler(Exception e) {
         log.error("exception: {}", e.getMessage());
         return R.error(ErrorCode.SYSTEM_ERROR, e.getMessage());
     }
