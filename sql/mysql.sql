61a771f89662e187e52f44273bb7a655f9b432fb
Ezhixuan <ezhixuanna@163.com>
2025-08-10 10:30:34 +0800

    feat(sql): 为 sys_user表添加索引
    - 在 sys_user 表中添加了两个索引：
      - 用户名称索引 (idx-name)
      - 用户账号索引 (idx-account)

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -8,6 +8,7 @@ use code_craft_ai_db;
 -- Table structure for sys_user
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
+DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user`
 (
     `id`          bigint                                                        NOT NULL AUTO_INCREMENT COMMENT '主键',
@@ -18,11 +19,13 @@ CREATE TABLE `sys_user`
     `profile`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci          DEFAULT NULL COMMENT '个人简介',
     `email`       varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci          DEFAULT NULL COMMENT '邮箱',
     `status`      tinyint                                                       NOT NULL DEFAULT '1' COMMENT '状态：1-正常，0-禁用',
-    `create_time` datetime                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` datetime                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+    `create_time` datetime                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` datetime                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `deleted`     tinyint                                                       NOT NULL DEFAULT '0' COMMENT '是否删除：0-未删除，1-已删除',
     `role`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户角色',
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    KEY `idx-name` (`name`) USING BTREE COMMENT '用户名称索引',
+    KEY `idx-account` (`account`) USING BTREE COMMENT '用户账号索引'
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
