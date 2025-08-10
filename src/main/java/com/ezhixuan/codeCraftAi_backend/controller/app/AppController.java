a2762dbd2b0d5c89b14fc48fee96acc96ea25b55
Ezhixuan <ezhixuanna@163.com>
2025-08-10 16:24:21 +0800

    feat(app): 增加应用更新和删除功能
    - 新增用户和管理员更新应用信息的接口
    - 新增删除应用的接口
    - 优化获取应用列表的接口，增加用户信息

--- src/main/java/com/ezhixuan/codeCraftAi_backend/controller/app/AppController.java
+++ src/main/java/com/ezhixuan/codeCraftAi_backend/controller/app/AppController.java
@@ -1,18 +1,26 @@
 package com.ezhixuan.codeCraftAi_backend.controller.app;
 
-import com.ezhixuan.codeCraftAi_backend.domain.entity.SysApp;
-import com.mybatisflex.core.paginate.Page;
+import static com.ezhixuan.codeCraftAi_backend.utils.UserUtil.getLoginUserId;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import com.ezhixuan.codeCraftAi_backend.annotation.AuthRole;
 import com.ezhixuan.codeCraftAi_backend.common.BaseResponse;
 import com.ezhixuan.codeCraftAi_backend.common.PageResponse;
 import com.ezhixuan.codeCraftAi_backend.common.R;
-import com.ezhixuan.codeCraftAi_backend.controller.app.vo.AppGenerateReqVo;
-import com.ezhixuan.codeCraftAi_backend.controller.app.vo.AppInfoCommonResVo;
-import com.ezhixuan.codeCraftAi_backend.controller.app.vo.AppQueryReqVo;
+import com.ezhixuan.codeCraftAi_backend.controller.app.vo.*;
+import com.ezhixuan.codeCraftAi_backend.domain.entity.SysApp;
+import com.ezhixuan.codeCraftAi_backend.exception.BusinessException;
+import com.ezhixuan.codeCraftAi_backend.exception.ErrorCode;
 import com.ezhixuan.codeCraftAi_backend.service.SysAppService;
 import com.ezhixuan.codeCraftAi_backend.service.SysUserService;
+import com.ezhixuan.codeCraftAi_backend.utils.UserUtil;
+import com.mybatisflex.core.paginate.Page;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -39,13 +47,45 @@ public class AppController {
     @GetMapping("/list")
     public PageResponse<AppInfoCommonResVo> getList(@RequestBody AppQueryReqVo queryReqVo) {
         Page<SysApp> sysAppPage = appService.getList(queryReqVo, true);
-        return R.list();
+        Set<Long> userIds = sysAppPage.getRecords().stream().map(SysApp::getUserId).collect(Collectors.toSet());
+        return R.list(appService.convert2Common(sysAppPage, userService.getUserInfoCommonMap(userIds)));
     }
 
-    @Operation(summary = "获取用户应用列表(用户)")
+    @Operation(summary = "获取用户应用列表(管理员)")
+    @AuthRole
     @GetMapping("/admin/list")
-    public PageResponse<AppInfoCommonResVo> adminGetList(@RequestBody AppQueryReqVo queryReqVo) {
+    public PageResponse<AppInfoAdminResVo> adminGetList(@RequestBody AppQueryReqVo queryReqVo) {
         Page<SysApp> sysAppPage = appService.getList(queryReqVo, false);
-        return R.list();
+        Set<Long> userIds = sysAppPage.getRecords().stream().map(SysApp::getUserId).collect(Collectors.toSet());
+        return R.list(appService.convert2Admin(sysAppPage, userService.getUserInfoAdminMap(userIds)));
+    }
+
+    @Operation(summary = "更新应用信息(用户)")
+    @PostMapping("/update")
+    public BaseResponse<Void> update(@RequestBody @Valid AppUpdateCommonReqVo updateReqVo) {
+        SysApp entity = updateReqVo.toEntity();
+        appService.updateById(entity);
+        return R.success();
+    }
+
+    @Operation(summary = "更新应用信息(管理员)")
+    @AuthRole
+    @PostMapping("/admin/update")
+    public BaseResponse<Void> adminUpdate(@RequestBody @Valid AppUpdateAdminReqVo updateReqVo) {
+        SysApp entity = updateReqVo.toEntity();
+        appService.updateById(entity);
+        return R.success();
+    }
+
+    @Operation(summary = "删除应用")
+    @DeleteMapping("/{id}")
+    public BaseResponse<Void> delete(@PathVariable Long id) {
+        if (!UserUtil.isAdmin()) {
+            if (!Objects.equals(getLoginUserId(), id)) {
+                throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+            }
+        }
+        appService.removeById(id);
+        return R.success();
     }
 }
---
+++ src/main/java/com/ezhixuan/codeCraftAi_backend/controller/app/vo/AppUpdateAdminReqVo.java
@@ -0,0 +1,37 @@
+package com.ezhixuan.codeCraftAi_backend.controller.app.vo;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class AppUpdateAdminReqVo extends AppConverter implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -8337583786116753335L;
+
+    @NotNull
+    @Schema(description = "应用ID")
+    private Long id;
+
+    @Size(min = 1, max = 12, message = "应用名称长度在 1 到 12 个字符之间")
+    @Schema(description = "应用名称")
+    private String name;
+
+    @Schema(description = "应用封面")
+    private String cover;
+
+    @Schema(description = "部署密钥")
+    private String deployKey;
+
+    @Min(value = 0, message = "应用优先级最小为 0")
+    @Schema(description = "优先级")
+    private Integer priority;
+}
